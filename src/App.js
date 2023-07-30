9e977a593ca8fa36f7200ef805abce0ce58aa0bd
lucasMarquesDevTi <lucasmarquesdevti@gmail.com>
2023-07-29 23:23:27 -0300

    criando-GlobalStyle-

--- src/App.js
+++ src/App.js
@@ -7,11 +7,6 @@ const AppContainer = styled.div`
       width: 100vw;
       height: 100vh;
       background-image: linear-gradient(90deg, #26ad8c 35%, #75eea3);
-    
-
-    li {
-      list-style: none;
-    }
 `
 
 function App() {
