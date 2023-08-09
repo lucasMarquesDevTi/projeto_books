201cae84160d938a4ebbd4a883e39328195b818b
lucas.marques <lucasmarquesdevti@gmail.com>
2023-08-09 00:15:15 -0300

    criando-props-ultimoslancamentos

---
+++ src/componentes/UltimosLancamentos/dadosUltimosLancamentos.js
@@ -0,0 +1,11 @@
+import livroConde from '../../imagens/conde de monte cristo.jpg'
+import livroPrincipe from'../../imagens/pequeno principe.jpg'
+import livroNapoleao from '../../imagens/napoleão.jpg'
+import livroArte from '../../imagens/arte.jpg'
+
+export const livros = [
+    {nome: 'O conde de monte cristo', src: livroConde, id: 1},
+    {nome: 'O pequeno príncipe', src: livroPrincipe, id: 2},
+    {nome: 'Napoleão', src: livroNapoleao, id: 3},
+    {nome: 'Arte da guerra', src: livroArte, id: 4}
+]
